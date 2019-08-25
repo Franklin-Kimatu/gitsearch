260bb30afa5f267cba13dcb1e566463ba142182e
Franklin-Kimatu <frankimatu@gmail.com>
2019-08-25 20:40:20 +0300

    production to true boolean

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   GITSEARCHAPIKEY:"23998e592593b005337d2544e6700fa73e21da39"
 };
 
